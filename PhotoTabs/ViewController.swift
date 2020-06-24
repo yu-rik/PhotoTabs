bcc136f2c96b4d25c33a9e2ee52e1f16e4571a7f
Yurii Nosachenko <yurik_n@outlook.com>
2020-06-24 16:59:50 +0300

    Initial Commit

---
+++ PhotoTabs/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PhotoTabs
+//
+//  Created by yurik on 6/24/20.
+//  Copyright © 2020 yurik. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
